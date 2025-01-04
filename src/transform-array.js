941a1b571e496382a2ac31ff4295647226f073cd
OlgaIndykova <ola1234567890dd@gmail.com>
2025-01-04 22:14:11 +0200

    fix: almost resolve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,53 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
+  if (!Array.isArray(arr)) throw Error('\'arr\' parameter must be an instance of the Array!');
   if (Array.isArray(arr)) {
     let newArr = [];
     for (let i = 0; i < arr.length; i += 1) {
-      if (arr[i] !== '--discard-prev' ||
-        arr[i] !== '--double-prev' ||
-        arr[i] !== '--double-next' ||
-        arr[i] !== '--discard-next') {
-        newArr.push(arr[i]);
+      switch (arr[i]) {
+        case '--discard-prev':
+          if (arr[i - 2] !== '--discard-next' && i > 0) {
+            newArr.pop();
+          };
+          break;
+        case '--double-prev':
+          if (i > 0) {
+            newArr.push(arr[i - 1]);
+          };
+          break;
+        case '--double-next':
+          if (i < arr.length - 1) {
+            newArr.push(arr[i + 1]);
+          };
+          break;
+        case '--discard-next':
+          if (arr[i - 1] === '--discard-next' && i < arr.length - 1) {
+            newArr.pop();
+          };
+          break;
+        default:
+          newArr.push(arr[i]);
       }
-      if (arr[i] === '--discard-prev' && i !== 0) {
-        newArr.pop(arr[i]);
-        newArr.pop(arr[i - 1]);
-      }
-      if (arr[i] === '--discard-prev' && i === 0) {
-        newArr.pop(arr[i]);
-      }
-      if (arr[i] === '--double-prev' && i !== 0) {
-        newArr.pop(arr[i]);
-        newArr.push(arr[i - 1]);
-      }
-      if (arr[i] === '--double-prev' && i === 0) {
-        newArr.pop(arr[i]);
-      }
-      if (arr[i] === '--double-next' && i !== arr.length - 1) {
-        newArr.pop(arr[i]);
-        newArr.push(arr[i + 1]);
-      }
-      if (arr[i] === '--double-next' && i === arr.length - 1) {
-        newArr.pop(arr[i]);
-      }
-      if (arr[i] === '--discard-next' && i !== arr.length - 1) {
-        newArr.pop(arr[i]);
-        newArr.pop(arr[i - 1]);
-      }
-      if (arr[i] === '--discard-next' && i === arr.length - 1) {
-        newArr.pop(arr[i]);
-      }
-      if (arr[i] === 1337 && arr[i+1] === 1337) {
-
-      newArr.pop();
-      newArr.pop();
-      
-      newArr.push(3);
-      } 
     }
     return newArr;
-  } else {return '\'arr\' parameter must be an instance of the Array!'}
+  };
 }
 
 module.exports = {
