209636d7047494359f08bcb025f722ffdaf00156
OlgaIndykova <ola1234567890dd@gmail.com>
2025-01-05 11:32:28 +0200

    feat: resolve encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,21 +10,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  // const arr = str.split('');
-  // let newArr = [];
+function encodeLine(str) {
+  const arr = str.split('');
+  let newStr = '';
   // let count = 0;
-  // for (let i = 0; i < arr.length; i += 1) {
-  //   while (arr[i+1] = arr[i]) {
-  //     count += 1;
-  //     arr[i] = arr[i+1];
-      
+  // let singleArr = Array.from(new Set(arr));
+  // for (let i = 0; i < singleArr.length; i += 1) {
+  //   for (let j = 0; j < arr.length; j += 1) {
+  //     if (singleArr[i] === arr[j]) {
+  //       count += 1;
+  //     };
   //   }
-  // newArr.push(count).push(arr[i]);
+  //   newStr += count + singleArr[i] ;
+  //   count = 0;
   // }
-  // return newArr.join('');
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // return newStr;
+  let count = 1;
+  let i;
+  for (i = 0; i < arr.length; i += 1) {
+    if (arr[i] === arr[i + 1]) {
+      count += 1;
+    }
+    else {
+      count = count > 1? count : '';
+      newStr += count + arr[i];
+      count = 1;
+    }  
+  }
+  return newStr;
 }
 
 module.exports = {
